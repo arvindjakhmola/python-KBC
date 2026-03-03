74dff57c15e169a44af013b1ad3ad8ca488227f2
Arvind Jakhmola <arvindjakhmola2009@gmail.com>
2026-03-03 23:20:06 +0530

    Update and rename day1 2.03.25.py to translator/translator.py
    just a simple translator

--- day1 2.03.25.py
+++
@@ -1,7 +0,0 @@
-# a="arvind"
-# print(a.upper) 
-# b = input("what is your name").upper()
-# print ("well hello there", b.translate("hindi")) 
-# print(b.rstrip("!@#$%^&*()_+"))
-# x = input("what is your name")
-# print(x.replace("harry", "cherry"))
